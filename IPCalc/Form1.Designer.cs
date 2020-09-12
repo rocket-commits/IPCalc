a5f477ee337c801ccce3012173e4cfbe8e75a368
Jorge A. Moreno Martínez <andresmo99@outlook.com>
2020-09-12 18:06:20 -0500

    Converter (tab3) working properly

--- IPCalc/Form1.Designer.cs
+++ IPCalc/Form1.Designer.cs
@@ -48,20 +48,29 @@
             this.tbBroadcast = new System.Windows.Forms.TextBox();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.tbFirstIP = new System.Windows.Forms.TextBox();
-            this.label8 = new System.Windows.Forms.Label();
-            this.tbLastIP = new System.Windows.Forms.TextBox();
-            this.label9 = new System.Windows.Forms.Label();
-            this.tbNumberHosts = new System.Windows.Forms.TextBox();
-            this.label10 = new System.Windows.Forms.Label();
-            this.btnProcess = new System.Windows.Forms.Button();
             this.tbMask = new System.Windows.Forms.TextBox();
             this.label11 = new System.Windows.Forms.Label();
-            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.tbNumberHosts = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.tbLastIP = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tbFirstIP = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.tbDecimal = new System.Windows.Forms.TextBox();
+            this.bgWorkerV4 = new System.ComponentModel.BackgroundWorker();
+            this.tbHex = new System.Windows.Forms.TextBox();
+            this.tbBinary = new System.Windows.Forms.TextBox();
+            this.bgWorkerConversion = new System.ComponentModel.BackgroundWorker();
+            this.btnConvert = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.tabPage3.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -86,7 +95,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(68, 17);
+            this.label2.Location = new System.Drawing.Point(79, 17);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(17, 13);
             this.label2.TabIndex = 2;
@@ -124,7 +133,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(37, 43);
+            this.label3.Location = new System.Drawing.Point(48, 43);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(48, 13);
             this.label3.TabIndex = 7;
@@ -133,7 +142,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(21, 95);
+            this.label4.Location = new System.Drawing.Point(32, 95);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(64, 13);
             this.label4.TabIndex = 8;
@@ -156,7 +165,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(54, 121);
+            this.label5.Location = new System.Drawing.Point(65, 121);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(31, 13);
             this.label5.TabIndex = 11;
@@ -165,7 +174,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(22, 147);
+            this.label6.Location = new System.Drawing.Point(33, 147);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(63, 13);
             this.label6.TabIndex = 12;
@@ -188,7 +197,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(22, 173);
+            this.label7.Location = new System.Drawing.Point(28, 173);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(68, 13);
             this.label7.TabIndex = 15;
@@ -247,6 +256,80 @@
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "IPv4";
             // 
+            // tbMask
+            // 
+            this.tbMask.Location = new System.Drawing.Point(109, 66);
+            this.tbMask.Name = "tbMask";
+            this.tbMask.Size = new System.Drawing.Size(357, 20);
+            this.tbMask.TabIndex = 24;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(63, 69);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(33, 13);
+            this.label11.TabIndex = 23;
+            this.label11.Text = "Mask";
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(391, 295);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess.TabIndex = 18;
+            this.btnProcess.Text = "Go";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // tbNumberHosts
+            // 
+            this.tbNumberHosts.Location = new System.Drawing.Point(109, 248);
+            this.tbNumberHosts.Name = "tbNumberHosts";
+            this.tbNumberHosts.Size = new System.Drawing.Size(357, 20);
+            this.tbNumberHosts.TabIndex = 22;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(17, 251);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(79, 13);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "Hosts available";
+            // 
+            // tbLastIP
+            // 
+            this.tbLastIP.Location = new System.Drawing.Point(109, 222);
+            this.tbLastIP.Name = "tbLastIP";
+            this.tbLastIP.Size = new System.Drawing.Size(357, 20);
+            this.tbLastIP.TabIndex = 20;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(56, 225);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(40, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Last IP";
+            // 
+            // tbFirstIP
+            // 
+            this.tbFirstIP.Location = new System.Drawing.Point(109, 196);
+            this.tbFirstIP.Name = "tbFirstIP";
+            this.tbFirstIP.Size = new System.Drawing.Size(357, 20);
+            this.tbFirstIP.TabIndex = 18;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(57, 199);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(39, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "First IP";
+            // 
             // tabPage2
             // 
             this.tabPage2.BackColor = System.Drawing.Color.White;
@@ -260,6 +343,13 @@
             // tabPage3
             // 
             this.tabPage3.BackColor = System.Drawing.Color.White;
+            this.tabPage3.Controls.Add(this.btnConvert);
+            this.tabPage3.Controls.Add(this.tbBinary);
+            this.tabPage3.Controls.Add(this.tbHex);
+            this.tabPage3.Controls.Add(this.label14);
+            this.tabPage3.Controls.Add(this.label13);
+            this.tabPage3.Controls.Add(this.label12);
+            this.tabPage3.Controls.Add(this.tbDecimal);
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
@@ -267,78 +357,76 @@
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Converter";
             // 
-            // tbFirstIP
+            // label14
             // 
-            this.tbFirstIP.Location = new System.Drawing.Point(109, 196);
-            this.tbFirstIP.Name = "tbFirstIP";
-            this.tbFirstIP.Size = new System.Drawing.Size(357, 20);
-            this.tbFirstIP.TabIndex = 18;
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(123, 104);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(36, 13);
+            this.label14.TabIndex = 3;
+            this.label14.Text = "Binary";
             // 
-            // label8
+            // label13
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(46, 199);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(39, 13);
-            this.label8.TabIndex = 17;
-            this.label8.Text = "First IP";
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(133, 78);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(26, 13);
+            this.label13.TabIndex = 2;
+            this.label13.Text = "Hex";
             // 
-            // tbLastIP
+            // label12
             // 
-            this.tbLastIP.Location = new System.Drawing.Point(109, 222);
-            this.tbLastIP.Name = "tbLastIP";
-            this.tbLastIP.Size = new System.Drawing.Size(357, 20);
-            this.tbLastIP.TabIndex = 20;
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(114, 52);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(45, 13);
+            this.label12.TabIndex = 1;
+            this.label12.Text = "Decimal";
             // 
-            // label9
+            // tbDecimal
             // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(45, 225);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(40, 13);
-            this.label9.TabIndex = 19;
-            this.label9.Text = "Last IP";
+            this.tbDecimal.Location = new System.Drawing.Point(176, 49);
+            this.tbDecimal.Name = "tbDecimal";
+            this.tbDecimal.Size = new System.Drawing.Size(187, 20);
+            this.tbDecimal.TabIndex = 0;
+            this.tbDecimal.TextChanged += new System.EventHandler(this.tbDecimal_TextChanged);
             // 
-            // tbNumberHosts
+            // bgWorkerV4
             // 
-            this.tbNumberHosts.Location = new System.Drawing.Point(109, 248);
-            this.tbNumberHosts.Name = "tbNumberHosts";
-            this.tbNumberHosts.Size = new System.Drawing.Size(357, 20);
-            this.tbNumberHosts.TabIndex = 22;
+            this.bgWorkerV4.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgWorkerV4_DoWork);
+            this.bgWorkerV4.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgWorkerV4_RunWorkerCompleted);
             // 
-            // label10
+            // tbHex
             // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(6, 251);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(79, 13);
-            this.label10.TabIndex = 21;
-            this.label10.Text = "Hosts available";
+            this.tbHex.Location = new System.Drawing.Point(176, 75);
+            this.tbHex.Name = "tbHex";
+            this.tbHex.Size = new System.Drawing.Size(187, 20);
+            this.tbHex.TabIndex = 4;
+            this.tbHex.TextChanged += new System.EventHandler(this.tbHex_TextChanged);
             // 
-            // btnProcess
+            // tbBinary
             // 
-            this.btnProcess.Location = new System.Drawing.Point(391, 295);
-            this.btnProcess.Name = "btnProcess";
-            this.btnProcess.Size = new System.Drawing.Size(75, 23);
-            this.btnProcess.TabIndex = 18;
-            this.btnProcess.Text = "Go";
-            this.btnProcess.UseVisualStyleBackColor = true;
+            this.tbBinary.Location = new System.Drawing.Point(176, 101);
+            this.tbBinary.Name = "tbBinary";
+            this.tbBinary.Size = new System.Drawing.Size(187, 20);
+            this.tbBinary.TabIndex = 5;
+            this.tbBinary.TextChanged += new System.EventHandler(this.tbBinary_TextChanged);
             // 
-            // tbMask
+            // bgWorkerConversion
             // 
-            this.tbMask.Location = new System.Drawing.Point(109, 66);
-            this.tbMask.Name = "tbMask";
-            this.tbMask.Size = new System.Drawing.Size(357, 20);
-            this.tbMask.TabIndex = 24;
+            this.bgWorkerConversion.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgWorkerConversion_DoWork);
+            this.bgWorkerConversion.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgWorkerConversion_RunWorkerCompleted);
             // 
-            // label11
+            // btnConvert
             // 
-            this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(52, 69);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(33, 13);
-            this.label11.TabIndex = 23;
-            this.label11.Text = "Mask";
+            this.btnConvert.Location = new System.Drawing.Point(288, 127);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 6;
+            this.btnConvert.Text = "Go";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
             // 
             // Form1
             // 
@@ -349,11 +437,15 @@
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "IP Calculator";
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -391,7 +483,15 @@
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.TextBox tbMask;
         private System.Windows.Forms.Label label11;
-        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.ComponentModel.BackgroundWorker bgWorkerV4;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox tbDecimal;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox tbBinary;
+        private System.Windows.Forms.TextBox tbHex;
+        private System.ComponentModel.BackgroundWorker bgWorkerConversion;
     }
 }
 
--- IPCalc/Form1.cs
+++ IPCalc/Form1.cs
@@ -15,6 +15,134 @@ namespace IPCalc
         public Form1()
         {
             InitializeComponent();
+            decNumber = null;
+            hexNumber = null;
+            binNumber = null;
+        }
+        public static string lastChanged { get; set; }
+        public static int? decNumber { get; set; }
+        public static string hexNumber { get; set; }
+        public static string binNumber { get; set; }
+
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            bool tbNull = CheckNullControls(tabPage3);
+            if (tbNull)
+            {
+                MessageBox.Show("Write at least one number in a text field to convert", "Info", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                if (!bgWorkerConversion.IsBusy)
+                {
+                    bgWorkerConversion.RunWorkerAsync();
+                }
+            }
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bgWorkerConversion_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                int dec;
+                string hexadec;
+                string binaary;
+                switch (lastChanged)
+                {
+                    case "tbDecimal":
+                        hexadec = string.Format("{0:x}", decNumber).ToUpper();
+                        hexNumber = hexadec;
+                        binaary = Convert.ToString((int)decNumber, 2);
+                        binaary = binaary.PadLeft(8, '0');
+                        binNumber = binaary;
+                        break;
+                    case "tbHex":
+                        dec = (int)Convert.ToInt64(hexNumber, 16);
+                        decNumber = dec;
+                        binaary = Convert.ToString((int)dec, 2);
+                        binaary = binaary.PadLeft(8, '0');
+                        binNumber = binaary;
+                        break;
+                    case "tbBinary":
+                        dec = Convert.ToInt32(binNumber, 2);
+                        decNumber = dec;
+                        hexadec = string.Format("{0:x}", dec).ToUpper();
+                        hexNumber = hexadec;
+                        break;
+                    default:
+                        break;
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void bgWorkerConversion_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            tbHex.Text = hexNumber;
+            tbBinary.Text = binNumber;
+            tbDecimal.Text = decNumber.ToString();
+        }
+
+        private void bgWorkerV4_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void bgWorkerV4_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+
+        private void tbDecimal_TextChanged(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(tbDecimal.Text))
+            {
+                decNumber = Convert.ToInt32(tbDecimal.Text);
+                lastChanged = "tbDecimal";
+            }
+            else
+            {
+                decNumber = null;
+            }
+        }
+
+        private void tbHex_TextChanged(object sender, EventArgs e)
+        {
+            hexNumber = tbHex.Text;
+            lastChanged = "tbHex";
+        }
+
+        private void tbBinary_TextChanged(object sender, EventArgs e)
+        {
+            binNumber = tbBinary.Text;
+            lastChanged = "tbBinary";
+        }
+
+        public static bool CheckNullControls(Control form)
+        {
+            bool isNull = true;
+            foreach (Control control in form.Controls)
+            {
+                if (control is TextBox)
+                {
+                    TextBox textBox = (TextBox)control;
+                    if (!String.IsNullOrEmpty(textBox.Text))
+                    {
+                        isNull = false;
+                    }
+                }
+            }
+            return isNull;
         }
     }
 }
